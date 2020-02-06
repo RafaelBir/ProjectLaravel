e0cf6ad8759f48ca85965dffcdec576502282d0d
@Rafael88 <rifatb01@gmail.com>
2020-02-06 23:00:48 +0300

    basic controller and route

---
+++ app/Http/Controllers/eventsDownloadedApiController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Providers\RouteServiceProvider;
+use App\User;
+use Illuminate\Foundation\Auth\RegistersUsers;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use function MongoDB\BSON\toJSON;
+
+class eventsDownloadedApiController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | eventsDownloaded Api Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles the response to the request of last 7 day's
+    | episode download list using a json response.
+    |
+    */
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest');
+    }
+
+    /**
+     * Get a validator for an incoming registration request.
+     *
+     * @param  array  $data
+     * @return \Illuminate\Contracts\Validation\Validator
+     */
+    protected function sevenDays()
+    {
+        return toJSON("hello");
+        /*return Validator::make($data, [
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'password' => ['required', 'string', 'min:8', 'confirmed'],
+        ]);*/
+    }
+
+}
